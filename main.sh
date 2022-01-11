119fe39125dbaa3c8e0069a14b36975582a2aca1
TitouanCharrier <70266642+TitouanCharrier@users.noreply.github.com>
2022-01-11 12:58:50 +0100

    whiptail_1

--- main.sh
+++ main.sh
@@ -1,1 +1,20 @@
-first commit
+#!/usr/bin/env bash
+
+export NEWT_COLORS='
+root=gray,gray
+shadow=gray,gray
+window=,gray
+border=white,gray
+textbox=white,red
+button=black,white
+'
+
+whiptail --title "Menu example" --menu "Choose an option" 25 78 16 \
+" <-- Return" "" \
+"Continent" "Choose a continent" \
+"Country" "Choose a country" \
+"Gheu" "Choose ta bonne grosse mère" \
+"Add Group" "Add a user group to the system." \
+"Modify Group" "Modify a group and its list of members." \
+"List Groups" "List all groups on the system."
+
