ea0350fc54b866ce1df3b827478184742a4e7d7b
TitouanCharrier <70266642+TitouanCharrier@users.noreply.github.com>
2022-01-11 22:11:16 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,20 +1,66 @@
 #!/usr/bin/env bash
 
-export NEWT_COLORS='
-root=gray,gray
-shadow=gray,gray
-window=,gray
-border=white,gray
-textbox=white,red
-button=black,white
-'
+# --- colors --- #
 
-whiptail --title "Menu example" --menu "Choose an option" 25 78 16 \
-" <-- Return" "" \
-"Continent" "Choose a continent" \
-"Country" "Choose a country" \
-"Gheu" "Choose ta bonne grosse mère" \
-"Add Group" "Add a user group to the system." \
-"Modify Group" "Modify a group and its list of members." \
-"List Groups" "List all groups on the system."
+#export NEWT_COLORS='
+#root=gray,gray
+#shadow=gray,gray
+#window=,gray
+#border=white,gray
+#textbox=white,red
+#button=black,white
+#'
+
+# --- functions --- #
+
+main_menu() {
+
+	CHOICE=$(whiptail --title "Main menu" --menu --notags "What do you want ?" 10 35 2 \
+	"1" "Generate a file tree"  \
+	"2" "Get a specific information"  \
+	3>&1 1>&2 2>&3)
+	
+	if [ $? -eq 1 ] ; then
+		exit
+	fi
+
+	case $CHOICE in	
+		"1" ) gene ;;
+		"2" ) info ;; 
+	esac
+}
+
+gene() {
+
+	CHOICE=$(whiptail --title "Generate a file tree" --inputbox  "Generate about :" 8 40 \
+	3>&1 1>&2 2>&3)
+
+	choice=${CHOICE,,}
+
+	if [ $? -eq 1 ] ; then
+		main_menu
+	fi
+
+}
+
+# --- beginning --- #
+
+i=0
+declare -A Continent_Consumption
+while IFS=, read -r -a line; do
+   for ((j = 0; j < ${#line[@]}; ++j)); do
+        Continent_Consumption[$i,$j]=${line[$j],,}
+    done
+    ((i++))
+done < ./src_files/Continent_Consumption_TWH.csv
+
+i=1
+while [ -n "${Continent_Consumption[0,$i]}" ] ; do
+	echo ${Continent_Consumption[0,$i]}
+	((i++))
+done
+
+main_menu
+
+
 
