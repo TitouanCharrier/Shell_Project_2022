a2ce6c502a14bfd89797a0b9534e931165e7bd78
TitouanCharrier <70266642+TitouanCharrier@users.noreply.github.com>
2022-01-14 14:54:14 +0100

    little fix

--- main.sh
+++ main.sh
@@ -15,9 +15,10 @@
 
 main_menu() {
 
-	CHOICE=$(whiptail --title "Main menu" --menu --notags "What do you want ?" 10 35 2 \
+	CHOICE=$(whiptail --title "Main menu" --menu --notags "What do you want ?" --cancel-button "Exit" 10 35 3 \
 	"1" "Generate a file tree"  \
-	"2" "Get a specific information"  \
+	"2" "Browse country / continents"  \
+	"3" "Get a specific information"  \
 	3>&1 1>&2 2>&3)
 
 	if [ $? -eq 1 ] ; then
@@ -25,12 +26,13 @@ main_menu() {
 	fi
 
 	case $CHOICE in
-		"1" ) gene ;;
-		"2" ) info ;;
+		"1" ) ask ;;
+		"2" ) browser ;;
+		"3" ) info ;;
 	esac
 }
 
-gene() {
+ask() {
 
 	CHOICE=$(whiptail --title "Generate a file tree" --inputbox  "Generate about :" 8 40 \
 	3>&1 1>&2 2>&3)
@@ -41,11 +43,21 @@ gene() {
 		main_menu
 	fi
 
+	test_entry $choice
+
+
+}
+
+test_entry() {
+
+	success=0
 	i=1
 	while [ -n "${Country_Consumption[0,$i]}" ] ; do
 		if [ "${Country_Consumption[0,$i]}" == $choice ] ; then
 			index=$i
-			print_country_col $index
+			mode="country"
+			((success++))
+			print_graph $index $mode
 		fi
 		((i++))
 	done
@@ -54,18 +66,130 @@ gene() {
 	while [ -n "${Continent_Consumption[0,$i]}" ] ; do
 		if [ "${Continent_Consumption[0,$i]}" == $choice ] ; then
 			index=$i
-			print_continent_col $index
+			mode="continent"
+			((success++))
+			print_graph $index $mode
 		fi
 		((i++))
 	done
+
+	if [ $success -eq 0 ] ; then
+		whiptail --msgbox --title "Error" "continent or country not referenced \n please try a different syntax" 10 40
+		main_menu
+	fi
 }
 
-print_country_col() {
+browser() {
+
+	CHOICE=$(whiptail --title "Main menu" --menu "What do you want ?" 30 35 20 \
+	"World" "" \
+	"OECD" "" \
+	"BRICS" "" \
+	"Europe" "" \
+	"North" "" \
+	"America" "" \
+	"Latin" "" \
+	"America" "" \
+	"Asia" "" \
+	"Pacific" "" \
+	"Africa"	"" \
+	"Middle-East" "" \
+	"CIS" "" \
+	"China" ""	\
+	"United States" "" \
+	"Brazil" "" \
+	"Belgium" "" \
+	"Czechia" "" \
+	"France" "" \
+	"Germany" "" \
+	"Italy" "" \
+	"Netherlands" "" \
+	"Poland" "" \
+	"Portugal" "" \
+	"Romania" "" \
+	"Spain" "" \
+	"Sweden" "" \
+	"United Kingdom" "" \
+	"Norway" "" \
+	"Turkey" "" \
+	"Kazakhstan" "" \
+	"Russia" "" \
+	"Ukraine" "" \
+	"Uzbekistan" "" \
+	"Argentina" "" \
+	"Canada" "" \
+	"Chile" "" \
+	"Colombia" "" \
+	"Mexico" "" \
+	"Venezuela" "" \
+	"Indonesia" "" \
+	"Japan" "" \
+	"Malaysia" "" \
+	"South Korea" "" \
+	"Taiwan" "" \
+	"Thailand" "" \
+	"India" "" \
+	"Australia" "" \
+	"New Zealand" "" \
+	"Algeria" "" \
+	"Egypt" "" \
+	"Nigeria" "" \
+	"South Africa" "" \
+	"Iran" "" \
+	"Kuwait" "" \
+	"Saudi Arabia" "" \
+	"United Arab Emirates" "" \
+	3>&1 1>&2 2>&3)
+
+	if [ $? -eq 1 ] ; then
+		main_menu
+	fi
+
+	choice=${CHOICE,,}
+
+	test_entry $choice
+}
+
+end() {
+	if (whiptail --title "Done" --yesno "Need anything more ?" --yes-button "Main menu" --no-button "Exit" 8 78) ; then
+		main_menu
+	else
+		exit
+	fi
+}
+
+print_graph() {
 	i=1
-	while [ -n "${Country_Consumption[$i,$index]}" ] ; do
-		echo ${Country_Consumption[$i,$index]}
-		((i++))
-	done
+	cat /dev/null > data_set.dat
+	cat /dev/null > temp.p
+
+	if [ "$mode" == "country" ] ; then
+
+		while [ -n "${Country_Consumption[$i,$index]}" ] ; do
+			echo -e "${Country_Consumption[$i,0]} \t ${Country_Consumption[$i,$index]}" >> data_set.dat
+			((i++))
+		done
+
+	else
+
+		while [ -n "${Continent_Consumption[$i,$index]}" ] ; do
+			echo -e "${Continent_Consumption[$i,0]} \t ${Continent_Consumption[$i,$index]}" >> data_set.dat
+			((i++))
+		done
+
+	fi
+
+	echo -e "set terminal png size 1920,1080 \n set output '$choice.png' \n set title 'Consumption of $CHOICE by years (TWH)' \n plot \"data_set.dat\" w lp " >> temp.p
+
+	mkdir -p results/$choice 
+
+	gnuplot temp.p
+
+	mv $choice.png results/$choice
+
+	rm temp.p data_set.dat
+
+	end
 }
 
 print_continent_col() {
@@ -74,6 +198,10 @@ print_continent_col() {
 		echo ${Continent_Consumption[$i,$index]}
 		((i++))
 	done
+}
+
+add_to_file() {
+	echo -e "${Continent_Consumption[$i,0]} \t ${Continent_Consumption[$i,$index]}"
 }
 
 # --- beginning --- #
@@ -101,7 +229,5 @@ while [ -n "${Continent_Consumption[0,$i]}" ] ; do
 	#echo ${Continent_Consumption[0,$i]}
 	((i++))
 done
-
-
 
 main_menu
